aa977b3620f29d05fa3662abfac4e24ba23d90b9
LookD <look_d@poczta.onet.pl>
2017-06-24 12:46:08 +0200

    init simple spring bootstrap

---
+++ src/main/java/com/lda/ZecMinerMonitor/ZecMinerMonitorApplication.java
@@ -0,0 +1,12 @@
+package com.lda.ZecMinerMonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZecMinerMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZecMinerMonitorApplication.class, args);
+	}
+}
