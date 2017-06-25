09fcdb8d6308ee7ab2348c1bf39fa672849926ea
ldatkiew <lda.datkiewicz@gmail.com>
2017-06-25 15:26:10 +0200

    implemented restart

--- src/main/java/com/lda/zecminer/monitor/restarter/Restarter.java
+++ src/main/java/com/lda/zecminer/monitor/restarter/Restarter.java
@@ -1,13 +1,21 @@
 package com.lda.zecminer.monitor.restarter;
 
+import java.io.IOException;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class Restarter {
 
-	public void restart() {
-		// TODO Auto-generated method stub
-		
-	}
+	private static final String RESTART_COMMAND = "shutdown.exe -r -t 0";
 
+	public void restart()
+	{
+		    try {
+				Runtime.getRuntime().exec(RESTART_COMMAND);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		    System.exit(0);
+	}
 }
