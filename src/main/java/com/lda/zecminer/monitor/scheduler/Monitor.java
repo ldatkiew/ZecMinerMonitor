d8ae62706aa4088abccc9327b3490fdf4351e746
ldatkiew <lda.datkiewicz@gmail.com>
2017-06-26 23:41:12 +0200

    monitor if the same gpu results in row

--- src/main/java/com/lda/zecminer/monitor/scheduler/Monitor.java
+++ src/main/java/com/lda/zecminer/monitor/scheduler/Monitor.java
@@ -13,38 +13,61 @@ import com.lda.zecminer.monitor.web.ZecMinerApiReader;
 @Component
 public class Monitor {
 
+	private static final int SAME_RESULT_IN_ROW_LIMIT = 4;
+	private static final int NOT_WORKING_IN_ROW_LIMIT = 1;
 	private static final Logger log = LoggerFactory.getLogger(Monitor.class);
-	
+
 	@Autowired
 	private ZecMinerApiReader apiReader;
-	
+
 	@Autowired
 	private RestarterI restarter;
+
 	
-	private static int notWorkingCounter = 0;
-	
-	@Scheduled(initialDelay = 3000, fixedRate = 5000)
-	public void checkMiners()
-	{
+	private int notWorkingCounter = 0;
+	private int sameResultCounter = 0;
+	private MinerStat previousMinerStat = null;
+
+	@Scheduled(initialDelay = 3000, fixedRate = 5000)  //TODO:LDA profile it
+	public void checkMiners() {
 		MinerStat minerStat = apiReader.readApi();
-		boolean allWorking = minerStat.isAllWorking();
-		
-		if(allWorking)
-		{
+
+		updateSameStatistics(minerStat);
+		updateAndLogWorkingStatistics(minerStat);
+
+		if (notWorkingCounter > NOT_WORKING_IN_ROW_LIMIT || sameResultCounter > SAME_RESULT_IN_ROW_LIMIT) {
+			log.error("Restart work station");
+			restarter.restart();
+		}
+	}
+
+	private void updateAndLogWorkingStatistics(MinerStat minerStat) {
+		if (minerStat.isAllWorking()) {
 			log.info("All GPU's works");
 			log.info(minerStat.forLog());
-		}
-		else
-		{
+			notWorkingCounter = 0;
+		} else {
 			log.error("Some GPU is not working. Restart work station");
 			log.error(minerStat.forLog());
 			notWorkingCounter++;
 		}
-		
-		if(notWorkingCounter > 1)
-		{
-			log.error("Restart work station");
-			restarter.restart();
+	}
+
+	private void updateSameStatistics(MinerStat minerStat) {
+		if (minerStat.equals(previousMinerStat)) {
+			sameResultCounter++;
+		} else {
+			sameResultCounter = 0;
 		}
+
+		previousMinerStat = minerStat;
+	}
+
+	public void setApiReader(ZecMinerApiReader apiReader) {
+		this.apiReader = apiReader;
+	}
+
+	public void setRestarter(RestarterI restarter) {
+		this.restarter = restarter;
 	}
 }
